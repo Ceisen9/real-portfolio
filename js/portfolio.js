c3854005acf12cbea98bb6f93ba9e2ab16fde89f
Craig Eisen <Ceisen91@gmail.com>
2016-04-19 21:31:08 -0400

    parralax is working, social media icons are in

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,11 +1,46 @@
 $(document).ready(function() {
   $('#myContainer').multiscroll({
-    sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE'],
+    sectionsColor: ['#FFFFF3', '#566270', '#566270', '#566270'],
     anchors: ['first', 'second', 'third', 'fourth'],
     menu: '#menu',
     navigation: true,
-    navigationTooltips: ['One', 'Two', 'Three', 'Four'],
-    loopBottom: true,
-    loopTop: true
+    navigationTooltips: ['Welcome', 'Work', 'About', 'Contact'],
+    loopBottom: false,
+    loopTop: false,
+    scrollingSpeed: 800,
+    touchSensitivity: 3,
   });
 });
+
+// $(document).ready(function() {
+//     $('#multiscroll').multiscroll({
+//         verticalCentered : true,
+//
+//         easing: 'easeInQuart',
+//         menu: false,
+//         sectionsColor: [],
+//         navigation: false,
+//         navigationPosition: 'right',
+//         navigationColor: '#000',
+//         navigationTooltips: [],
+//         loopBottom: false,
+//         loopTop: false,
+//         css3: false,
+//         paddingTop: 0,
+//         paddingBottom: 0,
+//         normalScrollElements: null,
+//         keyboardScrolling: true,
+//         touchSensitivity: 5,
+//
+//         // Custom selectors
+//         sectionSelector: '.ms-section',
+//         leftSelector: '.ms-left',
+//         rightSelector: '.ms-right',
+//
+//         //events
+//         onLeave: function(index, nextIndex, direction){},
+//         afterLoad: function(anchorLink, index){},
+//         afterRender: function(){},
+//         afterResize: function(){},
+//     });
+// });
